79d261d49c59359430d392f38815d1a473c8f3e5
Ijem <ij.ofili@gmail.com>
2015-05-30 04:25:12 +0100

    js date parser

--- clockIn.js
+++ clockIn.js
@@ -1,1 +1,23 @@
-.//
+/*function my_curr_date() {      
+    var currentDate = new Date()
+    var day = currentDate.getDate();
+    var month = currentDate.getMonth() + 1;
+    var year = currentDate.getFullYear();
+    var my_date = month+"-"+day+"-"+year;
+    document.getElementById("shell").value = my_date;    
+}
+*/
+
+var wRite = function(){
+    var dateTime = new Date();
+    var day = dateTime.getDate();
+    var month = dateTime.getMonth() + 1;
+    var year = dateTime.getFullYear();
+    var hour = dateTime.getHour();
+    var minute = dateTime.getMinute();
+    var second= dateTime.getSecond();
+    
+    var myDate = day+"-"+month+"-"+year;
+
+    document.getElementById("").innerHTML = myDate;
+}
