a5380e0580da7c68a964bdf1e9983cfd68451b63
Ijem <ij.ofili@gmail.com>
2015-05-30 15:40:43 +0100

    added styling

--- clockIn.js
+++ clockIn.js
@@ -1,23 +1,23 @@
-/*function my_curr_date() {      
-    var currentDate = new Date()
-    var day = currentDate.getDate();
-    var month = currentDate.getMonth() + 1;
-    var year = currentDate.getFullYear();
-    var my_date = month+"-"+day+"-"+year;
-    document.getElementById("shell").value = my_date;    
-}
-*/
 
 var wRite = function(){
     var dateTime = new Date();
     var day = dateTime.getDate();
     var month = dateTime.getMonth() + 1;
     var year = dateTime.getFullYear();
-    var hour = dateTime.getHour();
-    var minute = dateTime.getMinute();
-    var second= dateTime.getSecond();
+    var hour = dateTime.getHours();
+    var minute = dateTime.getMinutes();
+    var second= dateTime.getSeconds();
     
-    var myDate = day+"-"+month+"-"+year;
+    var myDate = day+"."+month+"."+year+" @ "+hour+":"+minute+":"+second;
 
-    document.getElementById("").innerHTML = myDate;
+    document.getElementById("shell").innerHTML = myDate;
+}
+
+//var eVent = function(){
+// alert(3445);
+//}
+
+var addComponent = function(){
+    document.createElement("table");
+    
 }
